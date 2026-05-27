87d8b56f2f48a371e939d6136bfeee237ca01d9d
IMurai <raihaan.tech@proton.me>
2026-05-27 21:30:08 +0700

    Add new main menu

--- exit_button.gd
+++ exit_button.gd
@@ -1,13 +1,9 @@
-extends Button
+extends TextureButton
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-
 func _on_pressed() -> void:
-	Transition.transition()
-	await Transition.on_transition_finished
-	get_tree().change_scene_to_file("res://cutscene_satu.tscn")
+	pass # Replace with function body.
---
+++ script/start_button.gd
@@ -0,0 +1,11 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _on_pressed() -> void:
+	Transition.transition()
+	await Transition.on_transition_finished
+	get_tree().change_scene_to_file("res://cutscene_satu.tscn")
